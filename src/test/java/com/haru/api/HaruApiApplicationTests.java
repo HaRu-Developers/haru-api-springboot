b975e992629a515f711d1df4b5a07441021c1983
2ghrms <aa020228@gmail.com>
2025-07-04 16:57:28 +0900

    feat/#1: Spring Boot 초기 세팅

---
+++ src/test/java/com/haru/api/HaruApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
