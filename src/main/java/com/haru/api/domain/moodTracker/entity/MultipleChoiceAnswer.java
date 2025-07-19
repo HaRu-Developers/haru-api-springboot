559643915e75dacb3e244c0d334573ded4e58acf
2ghrms <aa020228@gmail.com>
2025-07-20 03:53:40 +0900

    feat/#58: 분위기 트래커 관련 엔티티 작성
    - 설문 질문 엔티티 작성
    - 체크박스 선지 엔티티 작성
    - 체크박스 답변 엔티티 작성
    - 객관식 선지 엔티티 작성
    - 객관식 답변 엔티티 작성
    - 주관식 답변 엔티티 작성

---
+++ src/main/java/com/haru/api/domain/moodTracker/entity/MultipleChoiceAnswer.java
@@ -0,0 +1,28 @@
+package com.haru.api.domain.moodTracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "multiple_choices_answer")
+@Getter
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class MultipleChoiceAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "multiple_choice_id", nullable = false)
+    private MultipleChoice multipleChoice;
+}
---
+++ src/main/java/com/haru/api/domain/moodTracker/entity/enums/QuestionType.java
@@ -0,0 +1,7 @@
+package com.haru.api.domain.moodTracker.entity.enums;
+
+public enum QuestionType {
+    SUBJECTIVE,
+    MULTIPLE_CHOICE,
+    CHECKBOX_CHOICE
+}
