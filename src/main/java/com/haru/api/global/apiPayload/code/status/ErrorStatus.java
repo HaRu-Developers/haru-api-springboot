09334ac618d8df2c4cc94bc9cc77feaea7bef9a7
2ghrms <aa020228@gmail.com>
2025-07-23 01:21:19 +0900

    feat/#59: 에러코드 추가
    - 설문 관련 에러코드 추가
    - 이메일 전송 실패 에러코드 추가

--- src/main/java/com/haru/api/global/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/haru/api/global/apiPayload/code/status/ErrorStatus.java
@@ -41,7 +41,13 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 분위기 트래커 관련 에러
     MOOD_TRACKER_NOT_FOUND(HttpStatus.BAD_REQUEST,"MOODTRACKER4001", "분위기 트래커가 없습니다."),
-    MOOD_TRACKER_MODIFY_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "MOODTRACKER4002", "분위기 트래커에 권한이 없습니다."),;
+    MOOD_TRACKER_MODIFY_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "MOODTRACKER4002", "분위기 트래커에 권한이 없습니다."),
+    SURVEY_QUESTION_NOT_FOUND(HttpStatus.BAD_REQUEST, "MOODTRACKER4003", "분위기 트래커 설문에 없는 질문입니다."),
+    SURVEY_ANSWER_REQUIRED(HttpStatus.BAD_REQUEST, "MOODTRACKER4004", "분위기 트래커 설문의 필수 응답이 누락되었습니다."),
+
+    // 메일 관련 에러
+    MAIL_SEND_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "MAIL500", "이메일 전송에 실패했습니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
