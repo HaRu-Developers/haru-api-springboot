667e3cf566d630d1994d89857cb18f53008e84c2
이석주 <nick981213@naver.com>
2025-08-04 15:39:02 +0900

    feat/#139: 문서 검색 API 버전 업데이트

--- src/main/java/com/haru/api/domain/moodTracker/repository/MoodTrackerRepository.java
+++ src/main/java/com/haru/api/domain/moodTracker/repository/MoodTrackerRepository.java
@@ -4,9 +4,11 @@ import com.haru.api.domain.moodTracker.entity.MoodTracker;
 import com.haru.api.domain.workspace.dto.WorkspaceResponseDTO;
 import com.haru.api.domain.workspace.entity.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,5 +27,10 @@ public interface MoodTrackerRepository extends JpaRepository<MoodTracker, Long> 
             "WHERE mt.workspace.id = :workspaceId " +
             "AND mt.createdAt BETWEEN :startDate AND :endDate")
     List<WorkspaceResponseDTO.DocumentCalendar> findAllDocumentForCalendars(Long workspaceId, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE MoodTracker m SET m.respondentsNum = m.respondentsNum + 1 WHERE m.id = :moodTrackerId")
+    void addRespondentsNum(Long moodTrackerId);
 
 }
