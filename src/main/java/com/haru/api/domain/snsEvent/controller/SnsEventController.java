b2399eb61968cce766faf77bcc5ff07780fff702
Jinho622 <aceho21@gmail.com>
2025-08-05 18:37:15 +0900

    feat/#157: SnsEvent API 버저닝 작성 완료

--- src/main/java/com/haru/api/domain/snsEvent/controller/SnsEventController.java
+++ src/main/java/com/haru/api/domain/snsEvent/controller/SnsEventController.java
@@ -19,8 +19,9 @@ public class SnsEventController {
     private final SnsEventQueryService snsEventQueryService;
 
     @Operation(
-            summary = "SNS 이벤트 생성 API",
-            description = "SNS 이벤트 생성 API입니다. Header에 access token을 넣고 Path Variable에는 workspaceId를 Request Body에 SNS 이벤트 정보를 담아 요청해주세요."
+            summary = "SNS 이벤트 생성 API [v1.0 (2025-08-05)]",
+            description = " # [v1.0 (2025-08-05)](https://www.notion.so/2265da7802c580e8b883e3e4481fd61d?v=2265da7802c5816ab095000cc1ddadca&p=2265da7802c580c49467fe1b3b5d0766&pm=s)" +
+                    " SNS 이벤트 생성 API입니다. Header에 access token을 넣고 Path Variable에는 workspaceId를 Request Body에 SNS 이벤트 정보를 담아 요청해주세요."
     )
     @PostMapping("/{workspaceId}")
     public ApiResponse<SnsEventResponseDTO.CreateSnsEventResponse> instagramOauthRedirectUri(
@@ -61,8 +62,9 @@ public class SnsEventController {
     }
 
     @Operation(
-            summary = "SNS 이벤트 리스트 조회 API",
-            description = "SNS 이벤트 리스트 조회 API입니다. Header에 access token을 넣고 Path Variable에는 workspaceId를 넣어 요청해주세요."
+            summary = "SNS 이벤트 리스트 조회 API [v1.0 (2025-08-05)]",
+            description = "# [v1.0 (2025-08-05)](https://www.notion.so/2265da7802c580e8b883e3e4481fd61d?v=2265da7802c5816ab095000cc1ddadca&p=2265da7802c5806b8088c79d33ee9a52&pm=s)" +
+                    " SNS 이벤트 리스트 조회 API입니다. Header에 access token을 넣고 Path Variable에는 workspaceId를 넣어 요청해주세요."
     )
     @GetMapping("/{workspaceId}/list")
     public ApiResponse<SnsEventResponseDTO.GetSnsEventListRequest> getSnsEventList(
@@ -75,8 +77,9 @@ public class SnsEventController {
     }
 
     @Operation(
-            summary = "SNS 이벤트명 수정 API",
-            description = "SNS 이벤트명 수정 API입니다. Header에 access token을 넣고 Path Variable에는 snsEvnetId를 Request Body에 SNS 이벤트 수정 정보(title)를 담아 요청해주세요."
+            summary = "SNS 이벤트명 수정 API [v1.0 (2025-08-05)]",
+            description = "# [v1.0 (2025-08-05)](https://www.notion.so/2265da7802c580e8b883e3e4481fd61d?v=2265da7802c5816ab095000cc1ddadca&p=22a5da7802c580d3bed7c57de0b88492&pm=s)" +
+                    " SNS 이벤트명 수정 API입니다. Header에 access token을 넣고 Path Variable에는 snsEvnetId를 Request Body에 SNS 이벤트 수정 정보(title)를 담아 요청해주세요."
     )
     @PatchMapping("/{snsEvnetId}")
     public ApiResponse<?> updateSnsEventTitle(
@@ -87,10 +90,11 @@ public class SnsEventController {
         snsEventCommandService.updateSnsEventTitle(userId, snsEvnetId, request);
         return ApiResponse.onSuccess("");
     }
-          
+
     @Operation(
-            summary = "SNS 이벤트 삭제 API",
-            description = "SNS 이벤트 삭제 API입니다. Header에 access token을 넣고 Path Variable에는 삭제할 SNS Event의 snsEvnetId를 담아 요청해주세요."
+            summary = "SNS 이벤트 삭제 API [v1.0 (2025-08-05)]",
+            description = "# [v1.0 (2025-08-05)](https://www.notion.so/2265da7802c580e8b883e3e4481fd61d?v=2265da7802c5816ab095000cc1ddadca&p=2265da7802c5809b84d3d8c09f95c36b&pm=s)" +
+                    " SNS 이벤트 삭제 API입니다. Header에 access token을 넣고 Path Variable에는 삭제할 SNS Event의 snsEvnetId를 담아 요청해주세요."
     )
     @DeleteMapping("/{snsEvnetId}")
     public ApiResponse<?> deleteSnsEvent(
@@ -102,8 +106,9 @@ public class SnsEventController {
     }
 
     @Operation(
-            summary = "SNS 이벤트 조회 API",
-            description = "[v1.0 (2025-08-04)] SNS 이벤트 조회 API입니다. Header에 access token을 넣고 Path Variable에는 snsEventId를 넣어 요청해주세요."
+            summary = "SNS 이벤트 조회 API [v1.0 (2025-08-05)]",
+            description = "# [v1.0 (2025-08-05)](https://www.notion.so/2265da7802c580e8b883e3e4481fd61d?v=2265da7802c5816ab095000cc1ddadca&p=2265da7802c580c29f17d5bb447eb496&pm=s)" +
+                    " SNS 이벤트 조회 API입니다. Header에 access token을 넣고 Path Variable에는 snsEventId를 넣어 요청해주세요."
     )
     @GetMapping("/{snsEventId}")
     public ApiResponse<SnsEventResponseDTO.GetSnsEventRequest> getSnsEvent(
