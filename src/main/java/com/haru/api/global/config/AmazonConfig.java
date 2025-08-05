d4d93abaa3b694716a1b7a5c02b252ffed50cc5e
임동재 <djlim1998@gmail.com>
2025-08-05 16:46:43 +0900

    feat/#143: S3 Manager 버젼 수정
    - config 수정

--- src/main/java/com/haru/api/global/config/AmazonConfig.java
+++ src/main/java/com/haru/api/global/config/AmazonConfig.java
@@ -1,23 +1,21 @@
 package com.haru.api.global.config;
 
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Getter
 public class AmazonConfig {
-
-
-    private AWSCredentials awsCredentials;
 
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
@@ -31,10 +29,10 @@ public class AmazonConfig {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    @PostConstruct
-    public void init() {
-        this.awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    public String getBucket() {
+        return bucket;
     }
+
 
     @Bean
     public AmazonS3 amazonS3() {
@@ -46,7 +44,25 @@ public class AmazonConfig {
     }
 
     @Bean
-    public AWSCredentialsProvider awsCredentialsProvider() {
-        return new AWSStaticCredentialsProvider(awsCredentials);
+    public StaticCredentialsProvider awsCredentialsProvider() {
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
     }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(awsCredentialsProvider())
+                .build();
+    }
+
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(awsCredentialsProvider())
+                .build();
+    }
+
 }
--- src/main/java/com/haru/api/infra/s3/AmazonS3Manager.java
+++ src/main/java/com/haru/api/infra/s3/AmazonS3Manager.java
@@ -1,17 +1,18 @@
 package com.haru.api.infra.s3;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.haru.api.global.common.entity.Uuid;
 import com.haru.api.global.config.AmazonConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+import java.time.Duration;
 import java.util.UUID;
 
 @Slf4j
@@ -19,35 +20,45 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AmazonS3Manager{
 
-    private final AmazonS3 amazonS3;
-
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
     private final AmazonConfig amazonConfig;
 
-    private final UuidRepository uuidRepository;
 
-    public String uploadFile(String keyName, MultipartFile file){
-        if (file == null || file.isEmpty()) {
-            return null;
-        }
-
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(file.getContentType());
-        metadata.setContentLength(file.getSize());
+    public String uploadFile(String keyName, MultipartFile file) {
 
         try {
-            // ACL 설정 부분을 제거합니다.
-            PutObjectRequest putObjectRequest = new PutObjectRequest(
-                    amazonConfig.getBucket(), keyName, file.getInputStream(), metadata
-            );
+            software.amazon.awssdk.services.s3.model.PutObjectRequest putObjectRequest =
+                    software.amazon.awssdk.services.s3.model.PutObjectRequest.builder()
+                            .bucket(amazonConfig.getBucket())
+                            .key(keyName)
+                            .contentType(file.getContentType())
+                            .contentLength(file.getSize())
+                            .build();
 
-            amazonS3.putObject(putObjectRequest);
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
         } catch (Exception e) {
             log.error("S3 파일 업로드에 실패했습니다. key: {}", keyName, e);
             throw new RuntimeException("S3 upload failed", e);
         }
+        return keyName;
+    }
 
-        return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+
+    public String generatePresignedUrl(String keyName) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(amazonConfig.getBucket())
+                .key(keyName)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
+        return presignedRequest.url().toString();
     }
 
     public String generateKeyName(String path, UUID uuid) {
