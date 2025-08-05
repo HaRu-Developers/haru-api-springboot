f374e1b8e08085554b4bc2ed28cb1d285cf5a816
2ghrms <aa020228@gmail.com>
2025-08-05 18:50:31 +0900

    feat/#148: 약관 조회 API 개발
    - not found 에러 상태 추가

--- src/main/java/com/haru/api/global/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/haru/api/global/apiPayload/code/status/ErrorStatus.java
@@ -71,7 +71,10 @@ public enum ErrorStatus implements BaseErrorCode {
     SNS_EVENT_NO_AUTHORITY(HttpStatus.UNAUTHORIZED, "SNS_EVENT4007", "인스타그램 이벤트에 대한 수정 권한이 없습니다."),
 
     // last opened 관련 에러
-    USER_DOCUMENT_LAST_OPENED_NOT_FOUND(HttpStatus.NOT_FOUND, "LASTOPENED4001", "해당 문서에 대한 마지막 조회 데이터가 존재하지 않습니다.");
+    USER_DOCUMENT_LAST_OPENED_NOT_FOUND(HttpStatus.NOT_FOUND, "LASTOPENED4001", "해당 문서에 대한 마지막 조회 데이터가 존재하지 않습니다."),
+
+    // 약관 관련 에러
+    TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "TERM4004", "요청한 약관이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
