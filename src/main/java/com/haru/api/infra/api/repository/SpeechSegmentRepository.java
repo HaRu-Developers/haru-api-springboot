f9d0cef95be180e219b2daf6a4c5cacebf89c3b2
임동재 <djlim1998@gmail.com>
2025-08-13 19:55:32 +0900

    feat/#213: SpeechSegmentRepository에 Meeting에 속한 모든 Segment 조회 메서드 추가

--- src/main/java/com/haru/api/infra/api/repository/SpeechSegmentRepository.java
+++ src/main/java/com/haru/api/infra/api/repository/SpeechSegmentRepository.java
@@ -1,7 +1,12 @@
 package com.haru.api.infra.api.repository;
 
+import com.haru.api.domain.meeting.entity.Meeting;
 import com.haru.api.infra.api.entity.SpeechSegment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpeechSegmentRepository extends JpaRepository<SpeechSegment, Long> {
+    // 특정 Meeting에 속한 모든 SpeechSegment를 조회하는 메서드
+    List<SpeechSegment> findByMeeting(Meeting meeting);
 }
